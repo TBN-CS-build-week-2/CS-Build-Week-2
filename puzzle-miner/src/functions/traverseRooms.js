55d78f11014da5269f6c059b458c9cd354647d66
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-10-22 17:57:51 -0500

    very close to backward traversal working

--- puzzle-miner/src/functions/traverseRooms.js
+++ puzzle-miner/src/functions/traverseRooms.js
@@ -1,58 +1,67 @@
 import axios from 'axios';
 
 function playerTravel(direction) {
-    setTimeout(() => {
-        const auth = `Token ${localStorage.getItem("key")}`
-        const options = {
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': auth
-            },
-            data: {
-                'direction': direction
-            }
-        }
-        console.log('traveled', options)
-        axios
-            .get(`${props.backendUrl}/api/adv/init/`, options)
-            .then(res => {
-                return res.data
-            })
+    const backendUrl = 'https://lambda-treasure-hunt.herokuapp.com'
+    let nextRoom = null;
+    // setTimeout(() => { }, 15000);
+    // setTimeout(() => null, 15000);
+    console.log('key', localStorage.getItem('key'))
+    const auth = `Token ${localStorage.getItem("key")}`
+    const options = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': auth
+        },
 
-    }, 15000);
-
+    }
+    console.log('traveled', options)
+    return axios
+        .post(`${backendUrl}/api/adv/move/`, { 'direction': direction }, options)
+        .then(result => {
+            console.log(result.data)
+            nextRoom = result.data
+            console.log('nextroom', nextRoom)
+            return nextRoom
+        })
 }
 
 function traverse(room, currentRooms) {
     // console.log(room)
     currentRooms[room.room_id] = room
-    console.log(currentRooms)
-    return currentRooms
+    // console.log(currentRooms)
+    // return currentRooms
 
     let traversalPath = []
     let visited = {}
     let currRoom = room
 
-    visited[room.room_id] = {}
+    visited[room.room_id] = { title: currRoom.title, coordinates: currRoom.coordinates }
+    visited[room.room_id]['exits'] = {}
     for (let i = 0; i < room.exits.length; i++) {
-        visited[room.room_id][room.exits[i]] = '?'
+        visited[room.room_id]['exits'][room.exits[i]] = '?'
     }
 
     // loop until all rooms have been explored (all rooms added to visited)
     // change to 500
-    while (visited.length !== 50) {
-        // get the current room, id, and exits
+    // while (visited.length !== 50) {
+    async function roomStep() {
 
+        if (visited.length === 15) {
+            return
+        }
+
+        // get the current room, id, and exits
+        console.log('currroom', currRoom)
         let roomId = currRoom.room_id
         let roomExits = currRoom.exits
 
         // create a list of unvisited rooms for the visited list
         let unvisited = []
-        visited[roomId].forEach(element => {
-            if (visited[roomId][element] == '?') {
+        for (let element in visited[roomId]['exits']) {
+            if (visited[roomId]['exits'][element] == '?') {
                 unvisited.push(element)
             }
-        });
+        };
         console.log(unvisited)
 
         if (unvisited.length > 0) {
@@ -61,43 +70,55 @@ function traverse(room, currentRooms) {
             let nextDirection = unvisited[nextDirectionIndex]
 
             // move to a new room
-            currRoom = playerTravel(nextDirection)
+            // console.log(nextDirection)
+            // console.log(currRoom)
+
+            currRoom = await playerTravel(nextDirection)
             let nextRoomId = currRoom.room_id
-            traversalPath.push(nextDirection)
             // set the previous room direction and new room direction accordingly
-            visited[roomId][nextDirection] = nextRoomId;
+            console.log(currRoom)
+            // setTimeout(() => console.log(currRoom), currRoom.cooldown * 1000);
+            // break;
+            visited[roomId]['exits'][nextDirection] = nextRoomId;
             // if new room not visited add it to visited
             if (!(nextRoomId in visited)) {
                 let nextExits = currRoom.exits
-                visited[nextRoomId] = {}
+                visited[nextRoomId] = { title: currRoom.title, coordinates: currRoom.coordinates }
+                visited[nextRoomId]['exits'] = {}
                 for (let i = 0; i < currRoom.exits.length; i++) {
-                    visited[currRoom.room_id][currRoom.exits[i]] = '?'
+                    visited[currRoom.room_id]['exits'][currRoom.exits[i]] = '?'
                 }
             }
             let reverseDirs = { "n": "s", "s": "n", "e": "w", "w": "e" }
             let reverseDir = reverseDirs[nextDirection]
-            visited[nextRoomId][reverseDir] = roomId
+            visited[nextRoomId]['exits'][reverseDir] = roomId
+            console.log(visited)
 
 
         } else {
             // run a search to return to the nearest room with an unexplored direction
-
+            console.log('dead end', roomId)
             let stack = []
             let visited_search = new Set()
             stack.push([roomId])
             let found = 0
             let foundPath = []
+            // console.log(stack, stack[0])
 
             while (stack.length > 0 && found === 0) {
                 let reverseTraversal = []
                 let path = stack.pop()
+                console.log(path, path[path.length - 1])
                 let node = path[path.length - 1]
-                let searchRoom = visited[node]
+                let searchRoom = visited[node]['exits']
+                console.log(searchRoom)
                 if (!(node in visited_search)) {
                     visited_search.add(node)
+                    console.log(visited_search)
                 }
 
                 for (let direction in searchRoom) {
+                    console.log('-------direction---', direction)
                     if (searchRoom[direction] === '?') {
                         foundPath = path
                         found += 1
@@ -109,7 +130,7 @@ function traverse(room, currentRooms) {
 
                 let exploredDirections = []
                 for (let direction in searchRoom) {
-
+                    console.log('e******direction---', direction)
                     if (searchRoom[direction] !== '?') {
                         let value = searchRoom[direction]
                         if (!(value in path)) {
@@ -117,18 +138,26 @@ function traverse(room, currentRooms) {
                         }
                     }
                 }
-
+                console.log('explored idrections', exploredDirections)
                 for (let direction in exploredDirections) {
+                    direction = exploredDirections[direction]
+                    console.log('direction---------', direction)
                     reverseTraversal.push(direction)
-                    new_path = [...path]
+                    let new_path = [...path]
+                    console.log('serach room direction', direction, searchRoom[direction])
                     new_path.push(searchRoom[direction])
+                    console.log('newpath', new_path)
                     stack.push(new_path)
                 }
             }
+            console.log(foundPath)
             let previous = foundPath[0]
             for (let pathRoom in foundPath) {
+                pathRoom = foundPath[pathRoom]
+                console.log('end-- pathroom', pathRoom, previous)
                 if (pathRoom != foundPath[0]) {
-                    for (let way in visited[previous]) {
+                    for (let way in visited[previous]['exits']) {
+                        console.log('end-- way', way)
                         if (visited[previous][way] == pathRoom) {
                             playerTravel(way)
                             traversalPath.push(way)
@@ -139,7 +168,10 @@ function traverse(room, currentRooms) {
             }
 
         }
+        setTimeout(() => roomStep(), currRoom.cooldown * 1000);
     }
+    roomStep()
+
     return traversalPath, visited;
 }
 
